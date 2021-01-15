65d8f309a948186000f5d24e9dad2a65ca719bb0
ParkSeryu <64573353+ParkSeryu@users.noreply.github.com>
2021-01-15 16:11:44 +0900

    1/15

--- app/src/main/java/com/parkseryu/witness/ui/home/HomeFragment.kt
+++ app/src/main/java/com/parkseryu/witness/ui/home/HomeFragment.kt
@@ -4,28 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.parkseryu.witness.R
+import com.parkseryu.witness.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var homeViewDataBinding: FragmentHomeBinding
+
+    companion object {
+        fun newInstance() = HomeFragment()
+    }
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        homeViewDataBinding = DataBindingUtil.inflate<FragmentHomeBinding>(
+            inflater,
+            R.layout.fragment_home,
+            container,
+            false
+        ).apply {
+            this.viewModel = homeViewModel
+            this.lifecycleOwner = this@HomeFragment
+        }
+        return homeViewDataBinding.root
     }
 }
